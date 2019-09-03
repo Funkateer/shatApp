a3f7c6d79d140dcf69448f0c1c4ba4bf15e938fd
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-09-03 19:51:21 +0200

    install react-navigation-stack

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
+// import react Navigation
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+
 // import the screens we want to navigate
 import Start from './components/Start';
 import Chat from './components/Chat';
-
-// import react Navigation
-import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 // Create the navigator with 2 screens
 const navigator = createStackNavigator({
